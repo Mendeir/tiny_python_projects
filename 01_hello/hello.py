863ef175e8a79ffd3be5c065ebe3a5fbf68614da
MG <mg-git.plumbic@8shield.net>
2022-05-21 03:44:47 -0400

    Add function docstring

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -7,17 +7,22 @@
 import argparse
 
 
+# -----------------------------------------------
 def get_args():
+    """Get the command-line arguments"""
     parser = argparse.ArgumentParser(description="Say Hello")
     parser.add_argument("-n", "--name", metavar="name",
                         default="World", help="Name to greet")
     return parser.parse_args()
 
 
+# ------------------------------------------------
 def main():
+    """Main Function"""
     args = get_args()
     print("Hello, " + args.name + "!")
 
 
+# ------------------------------------------------
 if __name__ == "__main__":
     main()
