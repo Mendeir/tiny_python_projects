d44a0d1843f4e221e45403efc13b1dd4cff6c974
MG <mg-git.plumbic@8shield.net>
2022-05-21 02:56:19 -0400

    Add command that will parse input from command line

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3 
 #Purpose: Say Hello
-print("Hello, World!")
+
+import argparse 
+
+parser = argparse.ArgumentParser(description="Say Hello")
+parser.add_argument("name", help="Name to greet")
+args = parser.parse_args()
+print("Hello, " + args.name + "!")
