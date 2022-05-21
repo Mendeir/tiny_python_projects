ed8f580fbebc030a0c506e5a310e468b2e6ed90d
MG <mg-git.plumbic@8shield.net>
2022-05-21 03:40:43 -0400

    Fix whitespace

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3 
-#Purpose: Say Hello
+#!/usr/bin/env python3
+# Purpose: Say Hello
 
-import argparse 
+import argparse
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Say Hello")
-    parser.add_argument("-n", "--name", metavar="name", 
+    parser.add_argument("-n", "--name", metavar="name",
                         default="World", help="Name to greet")
     return parser.parse_args()
+
 
 def main():
     args = get_args()
